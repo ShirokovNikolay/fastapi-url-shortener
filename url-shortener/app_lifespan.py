3526437d57b4861df87b02ea91d16fbe6d9259cc
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-19 21:54:35 +0300

    Add lifespan function using init_from_state.

---
+++ url-shortener/app_lifespan.py
@@ -0,0 +1,16 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from api.api_v1.short_urls.crud import storage
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Действия до запуска приложения.
+    storage.init_storage_from_state()
+    # Ставим функцию на паузу на время
+    # работы приложения.
+    yield
+    # Действия перед завершением работы
+    # приложения.
