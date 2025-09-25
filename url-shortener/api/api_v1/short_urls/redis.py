9be2f341d64deeffb5b851ee02eecb4237f92586
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-25 17:38:02 +0300

    Create abstract class AbstractTokensHelper and wrapper RedisTokensHelper for interacting with redis.

--- url-shortener/api/api_v1/short_urls/redis.py
+++ url-shortener/api/api_v1/short_urls/redis.py
@@ -1,10 +1,82 @@
+import secrets
+from abc import ABC, abstractmethod
+
 from redis import Redis
 
 from core import config
 
-redis_tokens = Redis(
+
+class AbstractTokensHelper(ABC):
+    """
+    Нужно от обертки:
+    - Проверять существование токена
+    - Генерировать токен
+    - Добавлять токен в хранилище
+    - Сгенерировать и сохранить токен в хранилище
+    """
+
+    @abstractmethod
+    def token_exists(
+        self,
+        token: str,
+    ) -> bool:
+        """
+        Проверка существования токена в хранилище.
+
+        :param token:
+        :return:
+        """
+
+    @classmethod
+    def generate_token(cls) -> str:
+        return secrets.token_urlsafe(16)
+
+    @abstractmethod
+    def add_token(self, token: str) -> None:
+        """
+        Добавление токена в хранилище.
+
+        :param token:
+        :return:
+        """
+
+    def generate_and_save_token(self) -> str:
+        token = self.generate_token()
+        self.add_token(token)
+        return token
+
+
+class RedisTokensHelper(AbstractTokensHelper):
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        db: int,
+        tokens_set_name: str,
+    ) -> None:
+        self.redis = Redis(
+            host=host,
+            port=port,
+            db=db,
+            decode_responses=True,
+        )
+        self.tokens_set = tokens_set_name
+
+    def token_exists(self, token: str) -> bool:
+        return bool(
+            self.redis.sismember(
+                self.tokens_set,
+                token,
+            )
+        )
+
+    def add_token(self, token: str) -> None:
+        self.redis.sadd(self.tokens_set, token)
+
+
+redis_tokens = RedisTokensHelper(
     host=config.REDIS_HOST,
     port=config.REDIS_PORT,
     db=config.REDIS_DB_TOKENS,
-    decode_responses=True,
+    tokens_set_name=config.REDIS_TOKENS_SET_NAME,
 )
