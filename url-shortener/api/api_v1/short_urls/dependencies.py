77d574a40dd811a7817afc4a384fafdc754fd698
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-25 15:20:33 +0300

    Check the availability of api tokens in redis using sismember in validate_api_token.

--- url-shortener/api/api_v1/short_urls/dependencies.py
+++ url-shortener/api/api_v1/short_urls/dependencies.py
@@ -17,11 +17,12 @@ from fastapi.security import (
 )
 
 from core.config import (
-    API_TOKENS,
+    REDIS_TOKENS_SET_NAME,
     USERS_DB,
 )
 from schemas.short_url import ShortUrl
 from .crud import storage
+from .redis import redis_tokens
 
 log = logging.getLogger(__name__)
 
@@ -74,7 +75,10 @@ def save_storage_state(
 def validate_api_token(
     api_token: HTTPAuthorizationCredentials,
 ) -> None:
-    if api_token.credentials not in API_TOKENS:
+    if not redis_tokens.sismember(
+        REDIS_TOKENS_SET_NAME,
+        api_token.credentials,
+    ):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid API token.",
