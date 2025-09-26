9ebeaaadf306121278080ba17e01059017b94f1f
Nikolay Shirokov <shirkol2006@mail.ru>
2025-09-26 17:08:50 +0300

    Add redis for short urls. Save new short-urls in redis(create method).

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -1,7 +1,9 @@
 import logging
 
 from pydantic import BaseModel, ValidationError
+from redis import Redis
 
+from core import config
 from schemas.short_url import (
     ShortUrl,
     ShortUrlCreate,
@@ -12,6 +14,13 @@ from schemas.short_url import (
 from core.config import SHORT_URLS_STORAGE_FILEPATH
 
 log = logging.getLogger(__name__)
+
+redis = Redis(
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=config.REDIS_DB_SHORT_URLS,
+    decode_responses=True,
+)
 
 
 class ShortUrlsStorage(BaseModel):
@@ -53,6 +62,11 @@ class ShortUrlsStorage(BaseModel):
     def create(self, short_url_in: ShortUrlCreate) -> ShortUrl:
         short_url = ShortUrl(**short_url_in.model_dump())
         self.slug_to_short_url[short_url.slug] = short_url
+        redis.hset(
+            name=config.REDIS_SHORT_URLS_HASH_NAME,
+            key=short_url.slug,
+            value=short_url.model_dump_json(),
+        )
         log.info("Created short url %s", short_url)
         return short_url
 
