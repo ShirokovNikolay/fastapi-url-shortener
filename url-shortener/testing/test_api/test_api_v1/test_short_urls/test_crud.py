9561dd8e663195dce22784b130b59f2e548f45ec
ShirokovNikolay <shirkol2006@mail.ru>
2025-10-05 22:05:42 +0300

    Add examination: switch redis port to database for test if environment variable TESTING equal to 1.

--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
@@ -1,15 +1,6 @@
-import random
-from unittest import TestCase
+from os import getenv
 
-
-def total(a: int, b: int) -> int:
-    return a + b
-
-
-class TotalTestCase(TestCase):
-    def test_total(self) -> None:
-        num_a = random.randint(1, 100)  # noqa: S311
-        num_b = random.randint(1, 100)  # noqa: S311
-        result = total(num_a, num_b)
-        expected_result = num_a + num_b
-        self.assertEqual(expected_result, result)
+if getenv("TESTING") != "1":
+    raise OSError(  # noqa: TRY003
+        "Environment is not ready for start",  # noqa: EM101
+    )
