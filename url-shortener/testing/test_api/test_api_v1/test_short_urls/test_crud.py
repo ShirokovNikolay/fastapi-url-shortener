f05b0b932894d9135a533196dff5749e1119ef2b
ShirokovNikolay <shirkol2006@mail.ru>
2025-10-06 17:32:31 +0300

    Fix code style.

--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
@@ -2,5 +2,5 @@ from os import getenv
 
 if getenv("TESTING") != "1":
     raise OSError(  # noqa: TRY003
-        "Environment is not ready for start",  # noqa: EM101
+        "Environment is not ready for start.",  # noqa: EM101
     )
