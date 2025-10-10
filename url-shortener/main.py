c4942f66b798319ec8bfab86fdb5452b195255b7
ShirokovNikolay <shirkol2006@mail.ru>
2025-10-11 00:38:40 +0300

    Move read_root view in module api/main_views.

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -2,10 +2,10 @@ import logging
 
 from fastapi import (
     FastAPI,
-    Request,
 )
 
 from api import router as api_router
+from api.main_views import router as main_router
 from api.redirect_views import router as router_redirect_views
 from app_lifespan import lifespan
 from core import config
@@ -19,20 +19,6 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+app.include_router(main_router)
 app.include_router(api_router)
 app.include_router(router_redirect_views)
-
-
-@app.get("/")
-def read_root(
-    request: Request,
-    name: str = "World",
-) -> dict[str, str]:
-    docs_url = request.url.replace(
-        path="/docs",
-        query="",
-    )
-    return {
-        "message": f"Hello {name}!",
-        "docs": str(docs_url),
-    }
