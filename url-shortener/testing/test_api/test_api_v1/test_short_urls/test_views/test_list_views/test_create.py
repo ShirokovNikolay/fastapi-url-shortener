a31d9bb609f94ee3f3bdb1b2c87cccb439f9032f
Nikolay <shirkol2006@mail.ru>
2025-10-19 15:24:53 +0300

    Create class TestCreateInvalidShortUrl: fixture short_url_create_values, test invalid_short_url_slug.

--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_views/test_list_views/test_create.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_views/test_list_views/test_create.py
@@ -1,11 +1,17 @@
 import random
 import string
+from typing import Any
 
+import pytest
+from _pytest.fixtures import SubRequest
 from starlette import status
 from starlette.testclient import TestClient
 
 from main import app
 from schemas.short_url import ShortUrl, ShortUrlCreate
+from testing.conftest import (
+    build_short_url_create_random_slug,
+)
 
 
 def test_create_short_url(auth_client: TestClient) -> None:
@@ -40,3 +46,50 @@ def test_create_short_url_already_exists(
     response_data = response.json()
     expected_error_detail = f"Short URL with slug={short_url.slug!r} already exists."
     assert response_data["detail"] == expected_error_detail, response_data
+
+
+class TestCreateInvalidShortUrl:
+    @pytest.fixture(
+        params=[
+            pytest.param(
+                (
+                    "s",
+                    "string_too_short",
+                ),
+                id="too-short-slug",
+            ),
+            pytest.param(
+                (
+                    "s" * 11,
+                    "string_too_long",
+                ),
+                id="too-long-slug",
+            ),
+        ],
+    )
+    def short_url_create_values(
+        self,
+        request: SubRequest,
+    ) -> tuple[dict[str, Any], str]:
+        build = build_short_url_create_random_slug()
+        data = build.model_dump(mode="json")
+        slug, err_type = request.param
+        data["slug"] = slug
+        return data, err_type
+
+    def test_invalid_short_url_slug(
+        self,
+        short_url_create_values: tuple[dict[str, Any], str],
+        auth_client: TestClient,
+    ) -> None:
+        url = app.url_path_for("create_short_url")
+        create_data, expected_error_type = short_url_create_values
+        response = auth_client.post(
+            url=url,
+            json=create_data,
+        )
+        assert (
+            response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+        ), response.text
+        error_detail = response.json()["detail"][0]
+        assert error_detail["type"] == expected_error_type, error_detail
